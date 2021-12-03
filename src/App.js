fa6cb3fd5f5e41fc8eef5d2ed1a1f9a1e82ed74a
ariful4082 <ariful4082@gmail.com>
2021-12-04 02:22:39 +0600

    create quize ui and fake data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,173 @@
-import { useState } from 'react';
-import './app.css'
+import { useState } from "react";
+import "./app.css";
+import Trivia from "./components/Trivia";
 function App() {
-  const [questionNumber, setQuestionNumebr]= useState(5)
+  const [questionNumber, setQuestionNumebr] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
 
   const moneyPyramid = [
-    {id:1, amount: '$ 100'},
-    {id:2, amount: '$ 200'},
-    {id:3, amount: '$ 300'},
-    {id:4, amount: '$ 500'},
-    {id:5, amount: '$ 1000'},
-    {id:6, amount: '$ 2000'},
-    {id:7, amount: '$ 4000'},
-    {id:8, amount: '$ 8000'},
-    {id:9, amount: '$ 16000'},
-    {id:10, amount: '$ 320000'},
-    {id:11, amount: '$ 640000'},
-    {id:12, amount: '$ 125000'},
-    {id:13, amount: '$ 250000'},
-    {id:14, amount: '$ 500000'},
-    {id:15, amount: '$ 1000000'}
-  ]
+    { id: 1, amount: "$ 100" },
+    { id: 2, amount: "$ 200" },
+    { id: 3, amount: "$ 300" },
+    { id: 4, amount: "$ 500" },
+    { id: 5, amount: "$ 1000" },
+    { id: 6, amount: "$ 2000" },
+    { id: 7, amount: "$ 4000" },
+    { id: 8, amount: "$ 8000" },
+    { id: 9, amount: "$ 16000" },
+    { id: 10, amount: "$ 320000" },
+    { id: 11, amount: "$ 640000" },
+    { id: 12, amount: "$ 125000" },
+    { id: 13, amount: "$ 250000" },
+    { id: 14, amount: "$ 500000" },
+    { id: 15, amount: "$ 1000000" },
+  ];
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specialize in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Clock",
+          correct: false,
+        },
+        {
+          text: "Television",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "What are doing everyday after wakeuup?",
+      answers: [
+        {
+          text: "Brash",
+          correct: true,
+        },
+        {
+          text: "Danch",
+          correct: false,
+        },
+        {
+          text: "Run",
+          correct: false,
+        },
+        {
+          text: "Bad Tolk",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "If are you class ten. How Old are you?",
+      answers: [
+        {
+          text: "05",
+          correct: false,
+        },
+        {
+          text: "25",
+          correct: false,
+        },
+        {
+          text: "20",
+          correct: false,
+        },
+        {
+          text: "16",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Ask to you anyone. How are you doing? What are you replie",
+      answers: [
+        {
+          text: "Bad",
+          correct: false,
+        },
+        {
+          text: "Vary",
+          correct: false,
+        },
+        {
+          text: "Doing Well",
+          correct: true,
+        },
+        {
+          text: "Not Good",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "What is the answer of Assalamu alikum? ",
+      answers: [
+        {
+          text: "Salam",
+          correct: false,
+        },
+        {
+          text: "Sham",
+          correct: true,
+        },
+        {
+          text: "Olikumussalam",
+          correct: false,
+        },
+        {
+          text: "Good Morning",
+          correct: false,
+        },
+      ],
+    },
+  ];
   return (
     <div className="app">
-     <div className="main">main</div>
-     <div className="pyramid">
-       <ul className="moneyList">
-         {
-           moneyPyramid.map(m=>
-            <li className={questionNumber === m.id ? "moneyListItem active": "moneyListItem "}>
-           <span className="moneyListItemNumber">{m.id}</span>
-            <span className="moneyListItemAmount"> {m.amount}</span>
-         </li>
-            ).reverse()
-         }
-         
-       </ul>
-     </div>
-      
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Trivia
+            data={data}
+            setTimeOut={setTimeout}
+            setQuestionNumebr={setQuestionNumebr}
+            questionNumber={questionNumber}
+          />
+        </div>
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid
+            .map((m) => (
+              <li
+                className={
+                  questionNumber === m.id
+                    ? "moneyListItem active"
+                    : "moneyListItem "
+                }
+              >
+                <span className="moneyListItemNumber">{m.id}</span>
+                <span className="moneyListItemAmount"> {m.amount}</span>
+              </li>
+            ))
+            .reverse()}
+        </ul>
+      </div>
     </div>
   );
 }
